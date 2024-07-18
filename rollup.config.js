63efd96ba87911b7a968191b776f862c198a4c7a
Henrik <henrik.berntsen@outlook.com>
2024-07-18 15:20:32 +0200

    Implemented rollup-plugin-dts

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,42 @@
 import AutoImport from 'unplugin-auto-import/rollup'
 import copy from 'rollup-plugin-copy'
 import esbuild from 'rollup-plugin-esbuild'
+import { dts } from "rollup-plugin-dts";
 
-export default {
-	input: {
-		index: 'src/index.ts',
-		cli: 'src/node/cli.ts',
+
+
+
+export default [
+	{
+		input: {
+			index: 'src/index.ts',
+			cli: 'src/node/cli.ts',
+		},
+		output: {
+			dir: 'dist',
+			format: 'esm'
+		},
+		plugins: [
+			AutoImport({
+				dts: true,
+				dirs: [
+					'./src/entities/**',
+				],
+			}),
+			copy({
+				targets: [
+					{ src: 'package.json', dest: 'dist' },
+					{ src: 'package-lock.json', dest: 'dist' },
+				],
+			}),
+			esbuild({
+				tsconfig: 'tsconfig.json',
+			}),
+		]
 	},
-	output: {
-		dir: 'dist',
-		format: 'esm'
-	},
-	plugins: [
-		AutoImport({
-			dts: true,
-			dirs: [
-				'./src/entities/**',
-			],
-		}),
-		copy({
-			targets: [
-				{ src: 'package.json', dest: 'dist' },
-				{ src: 'package-lock.json', dest: 'dist' },
-			],
-		}),
-		esbuild(),
-	]
-};
+	{
+		input: "./src/index.ts",
+		output: [{ file: "dist/index.d.ts", format: "es" }],
+		plugins: [dts()],
+	}
+];
